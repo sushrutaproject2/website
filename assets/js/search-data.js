826885f51bcc821e937862a6179d724bea630230
wujastyk <wujastyk@gmail.com>
2025-07-16 19:44:26 +0000

    Deploying to gh-pages from @ sushrutaproject2/website@1dfc5041df68e4482e877f9447cc8646219600ec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/website/projects/";
           },
+        },{id: "nav-people",
+          title: "people",
+          description: "project participants",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/website/people/";
+          },
         },{id: "post-acquiring-manuscript-images",
         
           title: "Acquiring manuscript images",
